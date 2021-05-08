1fa632e86a31f2e60c36f0cbbc3b2084c7504aad
Aris Chow <arischow@gmail.com>
2021-05-08 22:59:38 +0800

    refactor(0001): remove built-in `enumerate`

--- python/0001_Two_Sum.py
+++ python/0001_Two_Sum.py
@@ -5,15 +5,21 @@ import pytest
 
 class Solution1:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for x_index, x_value in enumerate(nums):
-            for y_index, y_value in enumerate(nums[x_index + 1:], start=x_index + 1):
-                if x_value + y_value == target:
+        x_index = 0
+        for x in nums:
+            y_index = x_index + 1
+            for y in nums[y_index:]:
+                if x + y == target:
                     return [x_index, y_index]
+                y_index += 1
+            x_index += 1
 
 
 @pytest.mark.parametrize('solution', [Solution1])
 @pytest.mark.parametrize('test_input, expected', [
     [dict(nums=[2, 7, 9, 11], target=9), [0, 1]],
+    [dict(nums=[3, 2, 4], target=6), [1, 2]],
+    [dict(nums=[3, 2, 3], target=6), [0, 2]],
 ])
 def test(solution, test_input, expected):
     assert solution().twoSum(**test_input) == expected
